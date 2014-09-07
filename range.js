af11c080febf79052d12d989e0fb77dd91ac4a27
Alex Coady <alexcoady@stinkdigital.com>
2014-09-07 12:44:01 +0100

    yuidoc

--- range.js
+++ range.js
@@ -1,3 +1,18 @@
+/**
+ *  Range maps one range of numbers to another
+ *
+ *  @function rangeFunc
+ *  @param {Number} inputFrom Start number of range
+ *  @param {Number} inputTo End number of range
+ *  @param {Number} inputFrom Start number of domain
+ *  @param {Number} inputTo End number of domain
+ *  @return {Function} Range function
+ *
+ *  Usage
+ *  
+ *  `var rangeX = rangeFunc(1,10,1,100);`
+ *  `var mappedNumber = rangeX(5)` // 50
+ */
 function rangeFunc ( inputFrom, inputTo, outputFrom, outputTo ) {
   
   var range, domain, multiplier;
